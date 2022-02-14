8702e88eeb8eb1d2de4e7acc08a8e28bd3a57f78
Andreas Hæstad <andrhae@stud.ntnu.no>
2022-02-14 16:45:05 +0100

    setup(#21): enviroment django template

---
+++ backend/schoolbudget/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+class School(models.Model):
+    responsibility = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+class Budget(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    date = models.DateField()
+    amount = models.FloatField()
+
+    class Meta:
+        unique_together = ('school', 'date')
+
+class Accounting(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    date = models.DateField()
+    amount = models.FloatField()
+
+    class Meta:
+        unique_together = ('school', 'date')
+
+class BudgetChange(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    date = models.DateField()
+    amount = models.FloatField()
+    
+    class Meta:
+        unique_together = ('budget', 'date')
+
+
+class Prognosis(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    date = models.DateField()
+    amount = models.FloatField()
+    
+    class Meta:
+        unique_together = ('budget', 'date')
