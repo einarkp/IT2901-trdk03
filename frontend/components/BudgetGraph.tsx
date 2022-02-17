fe119d9c77c93137d3c2726e8ebd40d981b7a3a0
Einar <einarkp@gmail.com>
2022-02-17 15:56:21 +0100

    add(#22): hide lines by clicking corresponding legend

--- frontend/components/BudgetGraph.tsx
+++ frontend/components/BudgetGraph.tsx
@@ -1,6 +1,7 @@
 import { format } from 'path';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Legend, ResponsiveContainer, Tooltip } from 'recharts';
+import { DataKey } from 'recharts/types/util/types';
 import { GraphDataProps, TotalBudgetData } from '../Interfaces';
 import styles from '../styles/BudgetGraph.module.css'
 
@@ -25,6 +26,10 @@ type IProps = {}
 
 const BudgetGraph = ({ data }: GraphDataProps) => {
 
+  const [hideBudget, setHideBudget] = useState(false);
+  const [hideAmount, setHideAmount] = useState(false);
+  const [hideAmountPrediction, setHideAmountPrediction] = useState(false);
+
   function createTotalBudgetData() {
     // Reformat data to include additional value "budget", which is the incremental sum of "amount" along the year. 
     // This forms the total budget.
@@ -47,17 +52,25 @@ const BudgetGraph = ({ data }: GraphDataProps) => {
     return totalBudgetData
   }
 
+  function handleHideLine(e: { dataKey?: DataKey<string>; }) {
+    const clickedLegend = e.dataKey
+    if (clickedLegend === "budget") setHideBudget(!hideBudget)
+    if (clickedLegend === "amount") setHideAmount(!hideAmount)
+    if (clickedLegend === "amountPrediction") setHideAmountPrediction(!hideAmountPrediction)
+  }
+
   return (
     <div>
       <ResponsiveContainer width="100%" height={400} aspect={1.2}>
         <LineChart className={styles.chart} data={createTotalBudgetData()} margin={{ top: 20, right: 50, bottom: 0, left: 20 }}>
-          <Line strokeWidth="5" type="monotone" dataKey="amount" stroke="blue" />
-          <Line strokeWidth="5" type="monotone" dataKey="budget" stroke="red" />
-          <Line strokeWidth="5" type="monotone" dataKey="amountPrediction" stroke="purple" strokeDasharray="5 5" />
+          <Line strokeWidth="5" type="monotone" dataKey="amount" stroke="blue" hide={hideAmount}/>
+          <Line strokeWidth="5" type="monotone" dataKey="budget" stroke="red"  hide={hideBudget} />
+          <Line strokeWidth="5" type="monotone" dataKey="amountPrediction" stroke="purple" strokeDasharray="5 5" hide={hideAmountPrediction}/>
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="date" tickFormatter={shortMonthFormatter} textAnchor="end" />
           <Tooltip labelFormatter={longMonthFormatter} />
           <YAxis tickFormatter={amountFormatter} />
+          <Legend verticalAlign="bottom" onClick={handleHideLine} />
         </LineChart>
       </ResponsiveContainer>
     </div>
